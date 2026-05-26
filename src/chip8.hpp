b2617daeee2aac5c7ac82dbccac1b1f3e8cdf6cd
Aryan <aryand3@uci.edu>
2026-05-26 01:33:40 -0700

    intialize chip8 variables

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -1,6 +1,23 @@
+#include <cstdint>
+
+
 
 class Chip8{
 public:
-    Chip8();    
+    Chip8();   
+    
+    void increment_program_counter();
 
+    uint16_t opcode;
+    uint8_t memory[4096];
+    uint8_t graphics[2048];
+    uint8_t registers[16];
+    uint16_t index;
+    uint16_t program_counter;
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+    uint16_t stack[16];
+    uint16_t stack_pointer;
+    uint8_t keys[16];
+    
 };
